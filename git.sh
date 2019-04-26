954145b55248f81582c542f67b42a8746923ecb6
mikel <plyx_46204@126.com>
2019-04-26 08:15:09 +0800

    git apply scripts

---
+++ git.sh
@@ -0,0 +1,9 @@
+####### git messings ########
+git config --global user.email "plyx_46204@126.com"
+git config --global user.name "mikel"
+echo "# Devops" >> REAMDE.md
+git init
+git add REAMDE.md
+git commit -m "first cimmit"
+git remote add orgin git@github.com:gitplyx/Devops.git
+git push -u orgin master
